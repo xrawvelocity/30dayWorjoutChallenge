7c4cdebe603eaf45d9fd6ad0f689f7141c67744c
Victor Fernandez <vmfs99@gmail.com>
2020-04-04 19:02:03 -0400

    i think i got it, gonna bug test, start reset all

--- src/App.js
+++ src/App.js
@@ -8,89 +8,239 @@ const basicHard = require("./challenges/basicHard");
 
 let curYear = new Date().getFullYear();
 
-let ids = JSON.parse(localStorage.getItem("ids"));
+let idsEasy = JSON.parse(localStorage.getItem("idsEasy"));
 
-if (!ids) ids = [0];
+if (!idsEasy) idsEasy = [0];
 
-let startTime = localStorage.getItem('startTime')
+let idsNormal = JSON.parse(localStorage.getItem("idsNormal"));
 
-let started;
+if (!idsNormal) idsNormal = [0];
 
-startTime ? started = true : started = false
+let idsHard = JSON.parse(localStorage.getItem("idsHard"));
+
+if (!idsHard) idsHard = [0];
+
+let startTimeEasy = localStorage.getItem("startTimeEasy");
+
+let startedEasy;
+
+startTimeEasy ? (startedEasy = true) : (startedEasy = false);
+
+let finishTimeEasy = localStorage.getItem("finishTimeEasy");
+
+let startTimeNormal = localStorage.getItem("startTimeNormal");
+
+let startedNormal;
+
+startTimeNormal ? (startedNormal = true) : (startedNormal = false);
+
+let finishTimeNormal = localStorage.getItem("finishTimeNormal");
+
+let startTimeHard = localStorage.getItem("startTimeHard");
+
+let startedHard;
+
+startTimeHard ? (startedHard = true) : (startedHard = false);
+
+let finishTimeHard = localStorage.getItem("finishTimeHard");
 
 export default class App extends Component {
   state = {
     difficulty: basicHard,
-    start: started,
-    ids: [...ids]
+    startEasy: startedEasy,
+    startNormal: startedNormal,
+    startHard: startedHard,
+    idsEasy: [...idsEasy],
+    idsNormal: [...idsNormal],
+    idsHard: [...idsHard],
+    allIds: [...idsEasy, ...idsNormal, ...idsHard]
   };
 
   componentDidMount() {
-    if (!ids) ids = [0];
+    if (!idsEasy) idsEasy = [0];
     else {
-      ids = JSON.parse(localStorage.getItem("ids"));
+      idsEasy = JSON.parse(localStorage.getItem("idsEasy"));
+    }
+    if (!idsNormal) idsNormal = [0];
+    else {
+      idsNormal = JSON.parse(localStorage.getItem("idsNormal"));
+    }
+    if (!idsHard) idsHard = [0];
+    else {
+      idsHard = JSON.parse(localStorage.getItem("idsHard"));
     }
   }
   renderWorkouts = () => {
     console.log(basicHard);
     return this.state.difficulty.map(eachDay => {
-      if (!eachDay.rest) {
-        return (
-          <div className="day">
-            <div
-              className={
-                this.state.ids.includes(eachDay.id)
-                  ? "day-checked"
-                  : "day-unchecked"
-              }
-            >
-              &#10004;
-            </div>
-            <h2>Day {eachDay.day}:</h2>
-            <p>Pushups: {eachDay.pushups}</p>
-            <p>Pullups: {eachDay.pullups}</p>
-            <p>Leg Raises: {eachDay.legraises}</p>
-            <p>Squats: {eachDay.squats}</p>
-            {this.state.start ? (
+      if (this.state.difficulty === basicEasy) {
+        if (!eachDay.rest) {
+          return (
+            <div className="day">
               <div
                 className={
-                  this.state.ids.includes(eachDay.id)
-                    ? "day-btn__hidden"
-                    : "day-btn"
+                  this.state.allIds.includes(eachDay.id)
+                    ? "day-checked"
+                    : "day-unchecked"
                 }
-                onClick={() => this.addToDone(eachDay)}
               >
-                Done
+                &#10004;
               </div>
-            ) : null}
-          </div>
-        );
-      } else {
-        return (
-          <div className="day">
-            <h2>Day {eachDay.day}:</h2>
-            <p>REST DAY</p>
-            <p>You've earned it!</p>
-          </div>
-        );
+              <h2>Day {eachDay.day}:</h2>
+              <p>Pushups: {eachDay.pushups}</p>
+              <p>Pullups: {eachDay.pullups}</p>
+              <p>Leg Raises: {eachDay.legraises}</p>
+              <p>Squats: {eachDay.squats}</p>
+              {this.state.startEasy ? (
+                <div
+                  className={
+                    this.state.allIds.includes(eachDay.id)
+                      ? "day-btn__hidden"
+                      : "day-btn"
+                  }
+                  onClick={() => this.addToDoneEasy(eachDay)}
+                >
+                  Done
+                </div>
+              ) : null}
+            </div>
+          );
+        } else {
+          return (
+            <div className="day">
+              <h2>Day {eachDay.day}:</h2>
+              <p>REST DAY</p>
+              <p>You've earned it!</p>
+            </div>
+          );
+        }
+      } else if (this.state.difficulty === basicNormal) {
+        if (!eachDay.rest) {
+          return (
+            <div className="day">
+              <div
+                className={
+                  this.state.allIds.includes(eachDay.id)
+                    ? "day-checked"
+                    : "day-unchecked"
+                }
+              >
+                &#10004;
+              </div>
+              <h2>Day {eachDay.day}:</h2>
+              <p>Pushups: {eachDay.pushups}</p>
+              <p>Pullups: {eachDay.pullups}</p>
+              <p>Leg Raises: {eachDay.legraises}</p>
+              <p>Squats: {eachDay.squats}</p>
+              {this.state.startNormal ? (
+                <div
+                  className={
+                    this.state.allIds.includes(eachDay.id)
+                      ? "day-btn__hidden"
+                      : "day-btn"
+                  }
+                  onClick={() => this.addToDoneNormal(eachDay)}
+                >
+                  Done
+                </div>
+              ) : null}
+            </div>
+          );
+        } else {
+          return (
+            <div className="day">
+              <h2>Day {eachDay.day}:</h2>
+              <p>REST DAY</p>
+              <p>You've earned it!</p>
+            </div>
+          );
+        }
+      } else if (this.state.difficulty === basicHard) {
+        if (!eachDay.rest) {
+          return (
+            <div className="day">
+              <div
+                className={
+                  this.state.allIds.includes(eachDay.id)
+                    ? "day-checked"
+                    : "day-unchecked"
+                }
+              >
+                &#10004;
+              </div>
+              <h2>Day {eachDay.day}:</h2>
+              <p>Pushups: {eachDay.pushups}</p>
+              <p>Pullups: {eachDay.pullups}</p>
+              <p>Leg Raises: {eachDay.legraises}</p>
+              <p>Squats: {eachDay.squats}</p>
+              {this.state.startHard ? (
+                <div
+                  className={
+                    this.state.allIds.includes(eachDay.id)
+                      ? "day-btn__hidden"
+                      : "day-btn"
+                  }
+                  onClick={() => this.addToDoneHard(eachDay)}
+                >
+                  Done
+                </div>
+              ) : null}
+            </div>
+          );
+        } else {
+          return (
+            <div className="day">
+              <h2>Day {eachDay.day}:</h2>
+              <p>REST DAY</p>
+              <p>You've earned it!</p>
+            </div>
+          );
+        }
       }
     });
   };
 
-  addToDone = async eachDay => {
-    let copyArray = [...this.state.ids];
+  addToDoneEasy = async eachDay => {
+    let copyArray = [...this.state.idsEasy];
     await copyArray.push(eachDay.id);
     let filteredArray = copyArray.filter(function(item, pos) {
       return copyArray.indexOf(item) === pos;
     });
     console.log(filteredArray);
-    await localStorage.setItem("ids", JSON.stringify(filteredArray));
+    await localStorage.setItem("idsEasy", JSON.stringify(filteredArray));
 
-    await this.setState({ ids: filteredArray });
-    console.log(this.state.ids);
+    await this.setState({ idsEasy: filteredArray });
+    await this.setState({allIds: [...this.state.idsEasy]})
+  };
+
+  addToDoneNormal = async eachDay => {
+    let copyArray = [...this.state.idsNormal];
+    await copyArray.push(eachDay.id);
+    let filteredArray = copyArray.filter(function(item, pos) {
+      return copyArray.indexOf(item) === pos;
+    });
+    console.log(filteredArray);
+    await localStorage.setItem("idsNormal", JSON.stringify(filteredArray));
+
+    await this.setState({ idsNormal: filteredArray });
+    await this.setState({allIds: [...this.state.idsNormal]})
+  };
+
+  addToDoneHard = async eachDay => {
+    let copyArray = [...this.state.idsHard];
+    await copyArray.push(eachDay.id);
+    let filteredArray = copyArray.filter(function(item, pos) {
+      return copyArray.indexOf(item) === pos;
+    });
+    console.log(filteredArray);
+    await localStorage.setItem("idsHard", JSON.stringify(filteredArray));
+
+    await this.setState({ idsHard: filteredArray });
+    await this.setState({allIds: [...this.state.idsHard]})
   };
 
   render() {
+    console.log(this.state.allIds);
     return (
       <Fragment>
         <nav>
@@ -130,44 +280,170 @@ export default class App extends Component {
           </div>
         </nav>
         <main>
-          <div className="main-top">
-            <section className="main-btns">
-              <div
-                className={this.state.start ? "main-btn__hidden" : "main-btn"}
-                onClick={async () => {
-                  let date = new Date().toLocaleString("en-US", {
-                    timeZone: "America/New_York"
-                  });
-                  await localStorage.setItem("startTime", date);
-
-                  this.setState({ start: true, time: date });
-                }}
-              >
-                START
-              </div>
-              <div
-                className="main-btn"
-                onClick={async () => {
-                  await localStorage.removeItem("ids");
-                  await localStorage.removeItem("startTime");
-                  if (!ids) ids = [0];
-                  else {
-                    ids = JSON.parse(localStorage.getItem("ids"));
+          {/* ON DIFFICULTY HARD */}
+          {this.state.difficulty === basicHard ? (
+            <div className="main-top">
+              <section className="main-btns">
+                <div
+                  className={
+                    this.state.startHard ? "main-btn__hidden" : "main-btn"
                   }
-                  this.setState({ start: false, ids: [0] });
-                }}
+                  onClick={async () => {
+                    let date = new Date().toLocaleString("en-US", {
+                      timeZone: "America/New_York"
+                    });
+                    await localStorage.setItem("startTimeHard", date);
+
+                    this.setState({ startHard: true, timeHard: date });
+                  }}
+                >
+                  START
+                </div>
+                <div
+                  className="main-btn"
+                  onClick={async () => {
+                    await localStorage.removeItem("idsHard");
+                    await localStorage.removeItem("startTimeHard");
+                    if (!idsHard) idsHard = [0];
+                    else {
+                      idsHard = JSON.parse(localStorage.getItem("idsHard"));
+                    }
+                    await this.setState({ startHard: false, idsHard: [0] });
+                    await this.setState({allIds: [...this.state.idsEasy, ...this.state.idsNormal]})
+                  }}
+                >
+                  RESET
+                </div>
+              </section>
+              <p
+                className={
+                  this.state.startHard ? "main-started" : "main-started__hidden"
+                }
               >
-                RESET
-              </div>
-            </section>
-            <p
-              className={
-                this.state.start ? "main-started" : "main-started__hidden"
-              }
-            >
-              Started Challenge at: {startTime}
-            </p>
-          </div>
+                Started Challenge at: {startTimeHard}
+              </p>
+              <p
+                className={
+                  this.state.finishHard
+                    ? "main-finished"
+                    : "main-finished__hidden"
+                }
+              >
+                Finished Challenge at: {finishTimeHard}
+              </p>
+            </div>
+          ) : null}
+
+          {/* ON DIFFICULTY NORMAL */}
+          {this.state.difficulty === basicNormal ? (
+            <div className="main-top">
+              <section className="main-btns">
+                <div
+                  className={
+                    this.state.startNormal ? "main-btn__hidden" : "main-btn"
+                  }
+                  onClick={async () => {
+                    let date = new Date().toLocaleString("en-US", {
+                      timeZone: "America/New_York"
+                    });
+                    await localStorage.setItem("startTimeNormal", date);
+
+                    this.setState({ startNormal: true, timeNormal: date });
+                  }}
+                >
+                  START
+                </div>
+                <div
+                  className="main-btn"
+                  onClick={async () => {
+                    await localStorage.removeItem("idsNormal");
+                    await localStorage.removeItem("startTimeNormal");
+                    if (!idsNormal) idsNormal = [0];
+                    else {
+                      idsNormal = JSON.parse(localStorage.getItem("idsNormal"));
+                    }
+                    await this.setState({ startNormal: false, idsNormal: [0] });
+                    await this.setState({allIds: [...this.state.idsEasy, ...this.state.idsHard]})
+                  }}
+                >
+                  RESET
+                </div>
+              </section>
+              <p
+                className={
+                  this.state.startNormal
+                    ? "main-started"
+                    : "main-started__hidden"
+                }
+              >
+                Started Challenge at: {startTimeNormal}
+              </p>
+              <p
+                className={
+                  this.state.finishNormal
+                    ? "main-finished"
+                    : "main-finished__hidden"
+                }
+              >
+                Finished Challenge at: {finishTimeNormal}
+              </p>
+            </div>
+          ) : null}
+
+          {/* ON DIFFICULTY EASY */}
+          {this.state.difficulty === basicEasy ? (
+            <div className="main-top">
+              <section className="main-btns">
+                <div
+                  className={
+                    this.state.startEasy ? "main-btn__hidden" : "main-btn"
+                  }
+                  onClick={async () => {
+                    let date = new Date().toLocaleString("en-US", {
+                      timeZone: "America/New_York"
+                    });
+                    await localStorage.setItem("startTimeEasy", date);
+
+                    this.setState({ startEasy: true, timeEasy: date });
+                  }}
+                >
+                  START
+                </div>
+                <div
+                  className="main-btn"
+                  onClick={async () => {
+                    await localStorage.removeItem("idsEasy");
+                    await localStorage.removeItem("startTimeEasy");
+                    if (!idsEasy) idsEasy = [0];
+                    else {
+                      idsEasy = JSON.parse(localStorage.getItem("idsEasy"));
+                    }
+                    await this.setState({ startEasy: false, idsEasy: [0] });
+                    await this.setState({allIds: [...this.state.idsNormal, ...this.state.idsHard]})
+                  }}
+                >
+                  RESET
+                </div>
+              </section>
+              <p
+                className={
+                  this.state.startEasy ? "main-started" : "main-started__hidden"
+                }
+              >
+                Started Challenge at: {startTimeEasy}
+              </p>
+              <p
+                className={
+                  this.state.finishEasy
+                    ? "main-finished"
+                    : "main-finished__hidden"
+                }
+              >
+                Finished Challenge at: {finishTimeEasy}
+              </p>
+            </div>
+          ) : null}
+
           <section className="grid">{this.renderWorkouts()}</section>
         </main>
         <footer>
